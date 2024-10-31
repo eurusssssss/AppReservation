91beba1876616e1455fabc618af3c077ca71db40
eurusssssss <221175@unamba.edu.pe>
2024-10-31 15:21:42 -0500

    primer commit

---
+++ src/main/java/com/example/Desarollo/DesarolloApplication.java
@@ -0,0 +1,13 @@
+package com.example.Desarollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarolloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Desarollo/DesarolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Desarollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
